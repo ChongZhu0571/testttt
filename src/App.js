8a7305ca369bd0067058dff1d3d2c78c20fd67a6
Chong <chongzhu0571@gmail.com>
2021-05-25 14:19:12 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const groceryList = [
+  {
+    name: 'apple',
+    quantity: 5,
+    id: 1,
+  },
+  {
+    name: 'orange',
+    quantity: 4,
+    id: 2,
+  },
+  {
+    name: 'cucumber',
+    quantity: 3,
+    id: 3,
+  },
+];
+
+const Header = (props) => {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>{props.title}</h1>
+      <span className='stats'>Items: {props.totalItems}</span>
+    </header>
+  );
+};
+
+const Grocery = (props) => {
+  return (
+    <div className='grocery'>
+      <span className='grocery-name'>{props.name}</span>
+      <div className='counter'>
+        <button className='counter-action decrement'> - </button>
+        <span className='counter-items'> {props.quantity}</span>
+        <button className='counter-action increment'> + </button>
+      </div>
     </div>
   );
-}
+};
+const App = (props) => {
+  return (
+    <div>
+      <Header
+        title='My Grocery List'
+        totalItems={props.initialGroceryList.length}
+      />
+      {props.initialGroceryList.map((grocery) => (
+        <Grocery name={grocery.name} quantity={grocery.quantity} />
+      ))}
+    </div>
+  );
+};
+
 
 export default App;
